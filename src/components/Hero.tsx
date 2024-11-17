af82702f7a3b4cce87955a04c113a5a7b9d96dd4
Rashedin-063 <rashedinislam.06@gmail.com>
2024-11-18 00:18:39 +0600

    Hero content updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,15 +4,14 @@ import React, { useEffect } from "react";
 import ProfileImg2 from '../../public/sketch.png';
 import Image from 'next/image';
 import { FlipWords } from './ui/FlipWords';
-import { motion } from 'framer-motion';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
 
   const words = [
-    'Front End Developer',
-    'MERN Stack Developer',
-    'Full Stack Developer',
+    'Seamless User Experiences',
+    'Aesthetic Frontend Designs',
+    'Innovative Web Applications',
   ];
 
 const Hero = () => {
@@ -37,15 +36,18 @@ return (
     {/* Left Content */}
     <div className='space-y-4 font-chakraPetch text-white-pearl text-center lg:text-start'>
       <h2
-        className='text-3xl md:text-4xl font-semibold'
+        className='text-3xl md:text-4xl font-semibold flex flex-col'
         data-aos='fade-down'
         data-aos-delay='100'
         data-aos-easing='ease-out-cubic'
       >
-        Hello there,
+        Hi, I&apos;m{' '}
+        <span className='font-bold text-4xl md:text-5xl text-transparent bg-clip-text bg-gradient-text'>
+          Rashedin Islam
+        </span>
       </h2>
 
-      <h2
+      {/* <h2
         className='text-3xl md:text-4xl font-semibold'
         data-aos='fade-right'
         data-aos-delay='300'
@@ -55,29 +57,28 @@ return (
         <span className='font-bold text-4xl md:text-5xl text-transparent bg-clip-text bg-gradient-text'>
           Rashedin Islam
         </span>
-      </h2>
+      </h2> */}
 
       <div
-        className='mt-4 text-xl font-semibold text-yellow-sunshine space-y-2'
+        className='mt-4 text-xl font-semibold text-yellow-sunshine space-y-1'
         data-aos='fade-left'
         data-aos-delay='500'
         data-aos-easing='ease-out-cubic'
       >
         <span className='text-lg text-white-pearl'>
-          Crafting seamless digital experiences as a
+         A digital artisan passionate about crafting:
         </span>
         <br />
         <FlipWords words={words} />
       </div>
 
       <p
-        className='text-sm lg:text-base text-g[#FBFBFB] opacity-25 max-w-lg'
+        className='text-sm lg:text-base text-[#FBFBFB] opacity-25 max-w-lg'
         data-aos='zoom-in'
         data-aos-easing='ease-out-cubic'
         data-aos-delay='700'
       >
-        Passionate about delivering creative and scalable web
-        solutions.
+        Frontend, MERN Stack, or Full Stack—Let&apos;s code the future together!
       </p>
 
       {/* Call-to-Actions */}
@@ -107,7 +108,7 @@ return (
         alt="Rashedin Islam's Profile Photo"
         placeholder='blur'
         priority
-        className='h-48 w-48 md:w-60 md:h-60 lg:h-[300px] lg:w-[300px] rounded-full border-4 border-[#A19C8E] object-cover p-2 hover:p-0 transition-all duration-300 ease-in'
+        className='h-48 w-48 md:w-60 md:h-60 lg:h-[330px] lg:w-[330px] rounded-full border-4 border-[#A19C8E] object-cover p-2 hover:p-0 transition-all duration-300 ease-in'
       />
     </div>
   </div>
